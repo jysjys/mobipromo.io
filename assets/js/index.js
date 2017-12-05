013031a27b39fc555ea7b2f8ea784500d132ee2b
ylkjcjq <691424873@qq.com>
2017-12-05 13:18:50 +0800

    v1.0

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,22 @@
 var ad = true, cdn = true;
         var btn_sw = true;
         $(".ad").click(function() {
+            console.log('hello')
             if(ad) {
                 ad = !ad
-                $(this).parent().animate({"height" : "665"}, 300);
-            } else {
-                ad = !ad
-                $(this).parent().animate({"height" : "665"}, 300);
-            }
-        });
-        $(".cdn").click(function() {
-            if(ad) {
-                ad = !ad
-                $(this).parent().animate({"height" : "auto"}, 300);
+                $(this).parent().animate({"height" : "665px"}, 300);
             } else {
                 ad = !ad
                 $(this).parent().animate({"height" : "295px"}, 300);
             }
         });
-        $("#btn_img").click(function() {
-            console.log('hello')
-            if(btn_sw) {
-                $(this).parent('.btnn').previousSbiling().animate({"height" : "80px"}, 300);
-            }else {
-                $(this).parent('.btnn').previousSbiling().animate({"height" : "160px"}, 300);
+        $(".cdn").click(function() {
+            if(ad) {
+                ad = !ad
+                $(this).parent().animate({"height" : "665px"}, 300);
+            } else {
+                ad = !ad
+                $(this).parent().animate({"height" : "295px"}, 300);
             }
         });
         $('.carousel').carousel('pause');
