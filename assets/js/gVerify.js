bb96d329822fca8178db4bb4581405d39809cd1e
HengliYang <1029884642@qq.com>
2018-04-04 19:53:32 +0800

    add pag

--- assets/js/gVerify.js
+++ assets/js/gVerify.js
@@ -3,8 +3,8 @@
 		this.options = { //默认options参数值
 			id: "", //容器Id
 			canvasId: "verifyCanvas", //canvas的ID
-			width: "100", //默认canvas宽度
-			height: "30", //默认canvas高度
+			width: "80", //默认canvas宽度
+			height: "40", //默认canvas高度
 			type: "blend", //图形验证码默认类型blend:数字字母混合类型、number:纯数字、letter:纯字母
 			code: ""
 		}
@@ -32,8 +32,8 @@
 		_init: function() {
 			var con = document.getElementById(this.options.id);
 			var canvas = document.createElement("canvas");
-			this.options.width = con.offsetWidth > 0 ? con.offsetWidth : "100";
-			this.options.height = con.offsetHeight > 0 ? con.offsetHeight : "30";
+			this.options.width = con.offsetWidth > 0 ? con.offsetWidth : "80";
+			this.options.height = con.offsetHeight > 0 ? con.offsetHeight : "40";
 			canvas.id = this.options.canvasId;
 			canvas.width = this.options.width;
 			canvas.height = this.options.height;
